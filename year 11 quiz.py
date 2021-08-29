f803befc1ae539a6d40ce58d1878f2fa85c77026
liam howden <62722441+mlvforce@users.noreply.github.com>
2021-08-30 08:51:53 +1200

    final update year 11 quiz

--- year 11 quiz.py
+++ year 11 quiz.py
@@ -38,6 +38,7 @@ else:
 # a message according to whether or not they got it right.
 
 
+print(" ")
 question_one=input(questions[0])
 if question_one ==(answers[0]) or question_one == "storing items" or question_one == "storing multiple items" or question_one == "for storing items"  :
   print("right!")
@@ -47,7 +48,9 @@ elif question_one !=(answers[0]) or question_one != "storing items":
 else:
     print ("wrong here is the answer " + answers[0])
 
+# i have made gaps between the qustions in this version as it makes it easyer to know what question is what (for me at least) 
 
+print(" ")
 question_2=input(questions[1])
 if question_2 ==(answers[1]) or question_2 == " printing items" or question_2 == "printing statements"  :
   print("goodwork!")
@@ -59,7 +62,7 @@ else:
 
 
 
-
+print(" ")
 #trial 2 won the trail votes and so i will use this type of code to make a looping system 
 correct = False
 while not correct:
@@ -73,7 +76,7 @@ while not correct:
         print("sorry thats wrong this is the answer" " " )
 
 
-
+print(" ")
 #question 4 there is no more loop as qustion 3 was the "break" question if you will qustion 6 will also have a loop
 question_4=input(questions[3])
 if question_one ==(answers[3]) or question_4 == "colon":
@@ -84,6 +87,8 @@ elif question_4 !=(answers[3]) or question_4 != "colon":
 else:
     print ("wrong here is the answer " + answers[3])
 
+
+print(" ")
 #this question is some what hard like qustion 4
 question_5=input(questions[4])
 if question_one ==(answers[4]) or question_5 == "hashtag":
@@ -95,6 +100,8 @@ else:
     print ("wrong here is the answer " + answers[4])
 
 
+
+print(" ")
 #looping again as it is question 6(NOTE this code was made using the python verson 3.9.5 so if you are using this in the future please update it unless i do many thanks)
 correct = False
 while not correct:
@@ -107,6 +114,9 @@ while not correct:
     elif question_6 !=(answers[1]) or question_6 != "3.9.4":
         print("sorry thats wrong this is the answer" " " )
 
+
+
+print(" " )
 # just more questions not anything to exciting 
 question_7=input(questions[6])
 if question_one ==(answers[6]) or question_7 == "shell":
@@ -118,6 +128,8 @@ else:
     print ("HOW!?!?!??  " + answers[6])
 
 
+
+print(" ")
 question_8=input(questions[7])
 if question_one ==(answers[7]) or question_8 == "python":
   print("LETS GOOOO!!!!!")
@@ -128,8 +140,9 @@ else:
     print ("sorry thats wrong " + answers[7])
 
 
+#this qustion i am using a print statement before the question starts to ask the people not to use google on the next two questions
+#and in this version i have got rid of the mutliple print(" ") as the space was to large
 
-print(" ")
 print(" ")
 print("this qustions are a lot harder to anwser but please dont google them thanks")
 
@@ -142,6 +155,8 @@ elif question_9 !=(answers[8]):
 else:
     print ("wrong here is the answer " + answers[8])
 
+
+
 print(" ")
 question_10=input(questions[9])
 if question_10 ==(answers[9])   :
@@ -151,6 +166,20 @@ elif question_10 !=(answers[9]):
     print("well you did your best " " " + answers[9])
 else:
     print ("wrong here is the answer " + answers[9])
+
+
+
+print(" ")
+print(" ")
+
+print("THANK YOU FOR PLAYING -by liam howden!")
+
+#to show the user their score
+print("your score is out of 10")
+print(user_points)
+
+
+#that is all i have done for this quiz thank you for reading my code and playing the quiz if you did play it many thanks liam howden 
 
 
 
